d180583883cbc516198dd16813007b5aa402fb95
baha <baha.dursun123@gmail.com>
2025-03-07 14:22:04 +0300

    Replace 'my_python_node.py' with 'test.py' and implement a basic ROS 2 node in 'test.py'

--- saha/scripts/test.py
+++ saha/scripts/test.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
 
+class MyNode(Node):
+    def __init__(self):
+        super().__init__('my_node')
+        self.get_logger().info('Hello from my_node!')
+
+def main():
+    rclpy.init()
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+    
